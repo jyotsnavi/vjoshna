e555e7dec2579fc38bb37ec45d875634812ee0e5
unknown <vvpreethi578@gmail.com>
2019-06-21 14:58:24 +0530

    first commit

---
+++ vjoshna/src/main/java/com/example/vjoshna/VjoshnaApplication.java
@@ -0,0 +1,13 @@
+package com.example.vjoshna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VjoshnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VjoshnaApplication.class, args);
+	}
+
+}
